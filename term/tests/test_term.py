0cdf64e4261ccc910d8acc68fae5025855f35819
Stefan H. Holek <stefan@epy.co.at>
2012-04-08 22:35:38 +0200

    Add tests for raw and cbreak mode switching.

--- term/tests/test_term.py
+++ term/tests/test_term.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 
 from termios import *
@@ -5,6 +6,63 @@ from term import *
 
 
 class TermTests(unittest.TestCase):
+
+    def setUp(self):
+        self.saved = tcgetattr(sys.stdin)
+
+    def tearDown(self):
+        tcsetattr(sys.stdin, TCSAFLUSH, self.saved)
+
+    def test_defaults(self):
+        mode = tcgetattr(sys.stdin)
+        self.assertEqual(mode[LFLAG] & ECHO, ECHO)
+        self.assertEqual(mode[LFLAG] & ICANON, ICANON)
+        self.assertEqual(mode[LFLAG] & IEXTEN, IEXTEN)
+        self.assertEqual(mode[LFLAG] & ISIG, ISIG)
+        self.assertEqual(mode[CC][VMIN], b'\x01')
+        self.assertEqual(mode[CC][VTIME], b'\x00')
+
+    def test_setraw(self):
+        setraw(sys.stdin, min=0, time=1)
+        mode = tcgetattr(sys.stdin)
+        self.assertEqual(mode[LFLAG] & ECHO, 0)
+        self.assertEqual(mode[LFLAG] & ICANON, 0)
+        self.assertEqual(mode[LFLAG] & IEXTEN, 0)
+        self.assertEqual(mode[LFLAG] & ISIG, 0)
+        self.assertEqual(mode[CC][VMIN], 0)
+        self.assertEqual(mode[CC][VTIME], 1)
+
+    def test_setcbreak(self):
+        setcbreak(sys.stdin, min=0, time=1)
+        mode = tcgetattr(sys.stdin)
+        self.assertEqual(mode[LFLAG] & ECHO, 0)
+        self.assertEqual(mode[LFLAG] & ICANON, 0)
+        self.assertEqual(mode[LFLAG] & IEXTEN, IEXTEN)
+        self.assertEqual(mode[LFLAG] & ISIG, ISIG)
+        self.assertEqual(mode[CC][VMIN], 0)
+        self.assertEqual(mode[CC][VTIME], 1)
+
+    def test_rawmode(self):
+        with rawmode(sys.stdin, min=0, time=1):
+            mode = tcgetattr(sys.stdin)
+            self.assertEqual(mode[LFLAG] & ECHO, 0)
+            self.assertEqual(mode[LFLAG] & ICANON, 0)
+            self.assertEqual(mode[LFLAG] & IEXTEN, 0)
+            self.assertEqual(mode[LFLAG] & ISIG, 0)
+            self.assertEqual(mode[CC][VMIN], 0)
+            self.assertEqual(mode[CC][VTIME], 1)
+        self.test_defaults()
+
+    def test_cbreakmode(self):
+        with cbreakmode(sys.stdin, min=0, time=1):
+            mode = tcgetattr(sys.stdin)
+            self.assertEqual(mode[LFLAG] & ECHO, 0)
+            self.assertEqual(mode[LFLAG] & ICANON, 0)
+            self.assertEqual(mode[LFLAG] & IEXTEN, IEXTEN)
+            self.assertEqual(mode[LFLAG] & ISIG, ISIG)
+            self.assertEqual(mode[CC][VMIN], 0)
+            self.assertEqual(mode[CC][VTIME], 1)
+        self.test_defaults()
 
     def test_getyx(self):
         row, col = getyx()
@@ -15,3 +73,4 @@ class TermTests(unittest.TestCase):
         maxrow, maxcol = getmaxyx()
         self.assertNotEqual(maxrow, 0)
         self.assertNotEqual(maxcol, 0)
+
