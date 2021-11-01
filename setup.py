fdd3727a18da1bd017445c6a74c3245d4df3ce6f
Stefan H. Holek <stefan@epy.co.at>
2021-11-02 00:22:45 +0100

    Move metadata to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-version = '3.0'
-
-setup(name='term',
-      version=version,
-      description='An enhanced version of the tty module',
-      long_description=open('README.rst').read() + '\n' +
-                       open('CHANGES.rst').read(),
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Python Software Foundation License',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ],
-      keywords='terminal tty raw cbreak opentty getyx cursor position',
-      author='Stefan H. Holek',
-      author_email='stefan@epy.co.at',
-      url='https://github.com/stefanholek/term',
-      project_urls={
-          'Documentation': 'https://term.readthedocs.io/en/stable',
-          'Issue Tracker': 'https://github.com/stefanholek/term/issues',
-          'Source Code': 'https://github.com/stefanholek/term',
-      },
-      license='PSF-2.0',
-      packages=find_packages(),
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-)
+setup()
