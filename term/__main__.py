6f65a234c2b8db67874fa91e5241b95b8323e211
Stefan H. Holek <stefan@epy.co.at>
2022-03-07 18:56:25 +0100

    More debug info.

--- term/__main__.py
+++ term/__main__.py
@@ -7,8 +7,11 @@ import term
 
 if __name__ == '__main__':
     print('term.getyx', term.getyx())
-    print('term.getbgcolor', term.getbgcolor())
+    print('term.getyx_stdin_stdout', term.getyx_stdin_stdout())
     print('term.getfgcolor', term.getfgcolor())
+    print('term.getbgcolor', term.getbgcolor())
+    print('term.getnumcolors', term.getnumcolors())
+    print('term.name', term.name())
     print('term.isxterm', term.isxterm())
     print('term.islightmode', term.islightmode())
     print('term.isdarkmode', term.isdarkmode())
